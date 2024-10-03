eeab9793a90548057d0714946037f9f98796b21e
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-03 15:53:43 +0200

    foreach on hotels array

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +49,21 @@
 
 <body>
     <main>
+        <ul>
+            <?php foreach ($hotels as $hotel) { ?>
+            <li>
+                <h2><?php echo $hotel["name"] ?></h2>
 
+                <h3><?php echo $hotel["description"] ?></h3>
+
+                <h3><?php echo $hotel["parking"] ?></h3>
+
+                <h3><?php echo $hotel["vote"] ?></h3>
+
+                <h3><?php echo $hotel["distance_to_center"] ?></h3>
+            </li>
+            <?php } ?>
+        </ul>
     </main>
 </body>
 
