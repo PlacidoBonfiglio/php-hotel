c619fc9078148404380670f655508b0794620ef0
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-04 10:39:07 +0200

    added buttons

--- index.php
+++ index.php
@@ -36,6 +36,11 @@ $hotels = [
         'distance_to_center' => 50
     ],
 ];
+
+$filteredHotels;
+
+if (isset($_GET))
+
 ?>
 
 <!DOCTYPE html>
@@ -59,27 +64,28 @@ $hotels = [
             <h1 class="text-center pt-5 pb-5 fw-bold">PHP Hotels</h1>
 
             <!-- FORM -->
-            <form class="pb-4">
-                <div class="row justify-content-center">
+            <form class="pb-4 d-flex" method="get">
+                <!-- Hotels rating form -->
+                <div class="col-2 me-5">
+                    <select class="form-select form-select-sm" aria-label="Small select example">
+                        <option value="all" selected>Hotels rating</option>
+                        <option value="1-star">1 Star</option>
+                        <option value="2-stars">2 Stars</option>
+                        <option value="4-stars">4 Stars</option>
+                        <option value="5-stars">5 Stars</option>
+                    </select>
+                </div>
 
-                    <!-- Parking form -->
-                    <div class="col-3">
-                        <select class="form-select form-select-sm" aria-label="Small select example">
-                            <option value="all" selected>Hotels parking lots</option>
-                            <option value="with-parking-lots">Parking lots only</option>
-                        </select>
-                    </div>
+                <!-- Parking form -->
+                <div class="form-check col-3">
+                    <label class="form-check-label" for="parking">Parking lots</label>
+                    <input class="form-check-input" type="checkbox" id="parking" name="parking">
+                </div>
 
-                    <!-- Hotels rating form -->
-                    <div class="col-3">
-                        <select class="form-select form-select-sm" aria-label="Small select example">
-                            <option value="all" selected>Hotels rating</option>
-                            <option value="1-star">1 Star</option>
-                            <option value="2-stars">2 Stars</option>
-                            <option value="4-stars">4 Stars</option>
-                            <option value="5-stars">5 Stars</option>
-                        </select>
-                    </div>
+                <!-- Buttons -->
+                <div class="text-end w-100">
+                    <button type="submit" class="btn btn-primary me-3">Send</button>
+                    <button type="reset" class="btn btn-warning">Reset</button>
                 </div>
             </form>
 
@@ -97,13 +103,13 @@ $hotels = [
 
                 <tbody>
                     <?php foreach ($hotels as $hotel) { ?>
-                    <tr>
-                        <td scope="col"> <?php echo $hotel["name"]; ?></td>
-                        <td scope="col"> <?php echo $hotel["description"]; ?></td>
-                        <td scope="col"> <?php echo $hotel["parking"]; ?></td>
-                        <td scope="col"> <?php echo $hotel["vote"]; ?></td>
-                        <td scope="col"> <?php echo $hotel["distance_to_center"]; ?></td>
-                    </tr>
+                        <tr>
+                            <td scope="col"> <?php echo $hotel["name"]; ?></td>
+                            <td scope="col"> <?php echo $hotel["description"]; ?></td>
+                            <td scope="col"> <?php echo $hotel["parking"]; ?></td>
+                            <td scope="col"> <?php echo $hotel["vote"]; ?></td>
+                            <td scope="col"> <?php echo $hotel["distance_to_center"]; ?></td>
+                        </tr>
                     <?php } ?>
                 </tbody>
             </table>
